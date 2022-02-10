414e5d0c2eea9b899257f35067a1f1082b9e6c66
MikhailKaptakov <Mkaptakov@mail.ru>
2022-02-10 20:35:49 +0300

    - rework MealsDB (part 2) - DB rework to nonstatic

--- src/main/java/ru/javawebinar/topjava/dao/MealDAO.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDAO.java
@@ -4,32 +4,37 @@ import ru.javawebinar.topjava.database.CRUDMealDB;
 import ru.javawebinar.topjava.database.MealsDB;
 import ru.javawebinar.topjava.model.Meal;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class MealDAO implements CRUDMealDB {
+    private final MealsDB connection;
+
+    public MealDAO() {
+        this.connection = MealsDB.getInstance();
+    }
+
     @Override
     public void add(Meal meal) {
-        MealsDB.add(meal.getDateTime(), meal.getDescription(), meal.getCalories());
+        connection.add(meal.getDateTime(), meal.getDescription(), meal.getCalories());
     }
 
     @Override
     public boolean delete(Integer id) {
-        return MealsDB.delete(id);
+        return connection.delete(id);
     }
 
     @Override
     public boolean update(Meal meal) {
-        return MealsDB.update(meal.getId(), meal.getDateTime(), meal.getDescription(), meal.getCalories());
+        return connection.update(meal.getId(), meal.getDateTime(), meal.getDescription(), meal.getCalories());
     }
 
     @Override
     public List<Meal> getAll() {
-        return MealsDB.getAll();
+        return connection.getAll();
     }
 
     @Override
     public Meal get(Integer id) {
-        return MealsDB.get(id);
+        return connection.get(id);
     }
 }
--- src/main/java/ru/javawebinar/topjava/database/MealsDB.java
+++ src/main/java/ru/javawebinar/topjava/database/MealsDB.java
@@ -4,74 +4,88 @@ import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MealsDB {
-    /**TODO
-     * переделать базу так, чтобы создавался один инстанс объект базы данных, методы следовательно - не статические.
-     * А в класе MealDAO, появится поле объекта бд, получаемое через getInstance. Инициализацию начальных данных
-     * проводить в отдельном методе init() в приватном конструкторе. Конструктор защитить от многопоточности.
-      */
-    private final static AtomicInteger counter = new AtomicInteger(0);
-    private final static List<Meal> mealsDB = Collections.synchronizedList(new ArrayList<>());
 
-    static {
+    private static volatile MealsDB mealsDB;
+    private final static AtomicInteger counter = new AtomicInteger(0);
+    private final List<Meal> meals;
+
+    private MealsDB() {
+        meals = new CopyOnWriteArrayList<>();
+        this.init();
+    }
+
+    public static MealsDB getInstance() {
+        MealsDB localInstance = mealsDB;
+        if (localInstance == null) {
+            synchronized (MealsDB.class) {
+                localInstance = mealsDB;
+                if (localInstance == null) {
+                    mealsDB = localInstance = new MealsDB();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    private void init() {
         add(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0),
                 "Завтрак", 500);
         add(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0),
                 "Обед", 1000);
         add(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0),
-                                "Ужин", 500);
+                "Ужин", 500);
         add(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0),
-                        "Еда на граничное значение", 100);
+                "Еда на граничное значение", 100);
         add(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0),
-                        "Завтрак", 1000);
+                "Завтрак", 1000);
         add(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0),
-                        "Обед", 500);
+                "Обед", 500);
         add(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0),
                 "Ужин", 410);
     }
 
-    public static void add(LocalDateTime dateTime, String description, int calories) {
+    public void add(LocalDateTime dateTime, String description, int calories) {
         Integer id = counter.incrementAndGet();
         Meal meal = new Meal(dateTime, description, calories);
         meal.setId(id);
-        mealsDB.add(meal);
+        meals.add(meal);
     }
 
-    public static Meal get(Integer id)  {
-        for (Meal meal : mealsDB) {
+    public Meal get(Integer id)  {
+        for (Meal meal : meals) {
             if (meal.getId().equals(id)) {
                 return meal;
             }
         }
         return null;
     }
-    public static boolean delete(Integer id) {
+    public boolean delete(Integer id) {
         Meal meal = get(id);
         if (meal == null) {
             return false;
         }
-        mealsDB.remove(meal);
+        meals.remove(meal);
         return true;
     }
 
-    public static boolean update(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public boolean update(Integer id, LocalDateTime dateTime, String description, int calories) {
         if (delete(id)) {
             Meal meal = new Meal(dateTime, description, calories);
             meal.setId(id);
-            mealsDB.add(meal);
+            meals.add(meal);
             return true;
         } else {
             return false;
         }
     }
 
-    public static List<Meal> getAll() {
-        return mealsDB;
+    public List<Meal> getAll() {
+        return meals;
     }
 
 
