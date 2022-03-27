082f21585c452851883f06cfc9b72df6d36fdf8e
MikhailKaptakov <Mkaptakov@mail.ru>
2022-03-27 11:44:16 +0300

    - added RootControllerTest.java getMeals(); - added MATCHER_MEAL_TO to MealTestData.java - added RootControllerTest.java -added ResourceControllerTest.getStyleCss()

--- src/test/java/ru/javawebinar/topjava/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/ResourceControllerTest.java
@@ -1,17 +1,12 @@
 package ru.javawebinar.topjava;
 
-import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.UserTestData.*;
-import static ru.javawebinar.topjava.MealTestData.*;
+
 
 public class ResourceControllerTest extends AbstractControllerTest {
 
